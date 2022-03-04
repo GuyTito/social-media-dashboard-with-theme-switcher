2f1c0dd8b6443f11f03239297d317272e3993e02
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-03-04 12:22:00 +0000

    implementing style guide (wip)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Lime-Green': 'hsl(163, 72%, 41%)',
+        'Bright-Red': 'hsl(356, 69%, 56%)',
+      },
+      fontFamily: {
+        sans : ["'Inter'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
